28e61124c1211e3d144fe89636f80126e7c4ea49
leonmangis <leon.man@igsl-group.com>
2023-03-20 16:11:36 +0800

    Update: CRLF to LF

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
 # echo "Value of ABS_SCRIPT_PATH: ${ABS_SCRIPT_PATH}"
 # echo "Value of ABS_DIRECTORY: ${ABS_DIRECTORY}"
 
-# . "${ABS_DIRECTORY}"/venv/bin/activate
+. "${ABS_DIRECTORY}"/venv/bin/activate
 python "${ABS_DIRECTORY}"/main.py --config default.ini
